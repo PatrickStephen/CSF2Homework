219591a15d8d9cc44d2189dc1aae501031546577
patrickstephenit@yahoo.com <patrickstephenit@yahoo.com>
2021-12-01 08:37:22 -0600

    Added Student

---
+++ ClassesLibrary/Student2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student2
+    {       
+        //Frugal / Fields
+        //private datatype _camelCase
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        //People / Properties
+        //public datatype PascalCase
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }//end FirstName
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }//end LastName
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }//end ID
+
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }//end Gpa
+
+        //Collect / Constructors (ctor)
+        //public ClassName(datatype paramerName)
+        public Student2(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }
+        public Student2() { }
+        public override string ToString()
+        {
+            return string.Format("Student is: {0} {1}\n" +
+                                 "ID: {2}\n" +
+                                 "GPA: {3:f}\n",
+                                 FirstName,
+                                 LastName,
+                                 Id,
+                                 Gpa);
+        }
+    }
+}
---
+++ TesterProgram/TesterPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassesLibrary;
+
+namespace TesterProgram
+{
+    class TesterPage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CSF2 Homework Packet");
+            Console.Title = "CSF2 Homework Lab";
+            Console.WriteLine();
+
+            #region Student
+            Student2 s1 = new Student2();
+            s1.FirstName = "John";
+            s1.LastName = "Wick";
+            s1.Id = "1234";
+            s1.Gpa = 3.5f;
+
+            Console.WriteLine($"{s1.FirstName} {s1.LastName}'s school ID # is {s1.Id}. They have a GPA of {s1.Gpa}");
+
+            //Student2 s2 = new Student2();
+            //s2.FirstName = "Harry";
+            //s2.LastName = "Potter";
+            //s2.Id = "4";
+            //s2.Gpa = 9.75f;
+
+            Student2 s2 = new Student2("Harry", "Potter", "4", 9.75f);
+            Console.WriteLine(s2);
+            #endregion
+
+
+
+        }
+    }
+}
